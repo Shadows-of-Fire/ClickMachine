68cf9af8cec790d357881e2c07cb7abda7f27ca1
Shadows_of_Fire <3682588+Shadows-of-Fire@users.noreply.github.com>
2025-07-10 00:46:16 -0700

    Update deps

--- src/main/java/dev/shadowsoffire/clickmachine/ClickMachineClient.java
+++ src/main/java/dev/shadowsoffire/clickmachine/ClickMachineClient.java
@@ -4,10 +4,9 @@ import dev.shadowsoffire.clickmachine.gui.ClickMachineScreen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.common.EventBusSubscriber.Bus;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-@EventBusSubscriber(modid = ClickMachine.MODID, value = Dist.CLIENT, bus = Bus.MOD)
+@EventBusSubscriber(modid = ClickMachine.MODID, value = Dist.CLIENT)
 public class ClickMachineClient {
 
     @SubscribeEvent
